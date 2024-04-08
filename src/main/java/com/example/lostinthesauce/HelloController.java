b6987086f07275e680ae7898f448d9c4c8659581
ianazimov <ianazimov@gmail.com>
2024-04-08 18:45:34 -0400

    New Method, just adds a button on the original screen to print out the accounts in the database, and also added a button to go back to account screen

--- src/main/java/com/example/lostinthesauce/HelloController.java
+++ src/main/java/com/example/lostinthesauce/HelloController.java
@@ -37,6 +37,10 @@ public class HelloController {
         }
     }
 
+    public void printDatabase(){
+        dbClass.printAccountInfo(connect);
+    }
+
 
     public void onLoginPress(ActionEvent event) throws IOException{
         String usernameInput = usernameTextfield.getText();
@@ -75,6 +79,14 @@ public class HelloController {
 
     public void changeScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("sceneChange.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void changeSceneBack(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
