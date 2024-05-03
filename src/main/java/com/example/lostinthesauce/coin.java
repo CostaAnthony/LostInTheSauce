10d60e34f85add67172489900ba4b723897ba0b5
Anthony <costa18@farmingdale.edu>
2024-05-03 15:30:57 -0400

    I added a coin counter for each level.

--- src/main/java/com/example/lostinthesauce/coin.java
+++ src/main/java/com/example/lostinthesauce/coin.java
@@ -6,10 +6,12 @@ import javafx.scene.shape.Circle;
 public class coin {
     private Circle circle;
     private boolean collected;
+    private int touchCount;
 
     public coin(double centerX, double centerY) {
         circle = new Circle(centerX, centerY, 20, Color.GOLD);
         this.collected = false;
+        this.touchCount = 0;
     }
 
     public Circle getCircle() {
@@ -23,6 +25,15 @@ public class coin {
     public void setCollected(boolean collected) {
         this.collected = collected;
         circle.setVisible(!collected);
+    }
+
+    public int getTouchCount() {
+        return touchCount;
+    }
+
+    public void incrementTouchCount() {
+        touchCount++;
+        System.out.println(touchCount);
     }
 
 }
