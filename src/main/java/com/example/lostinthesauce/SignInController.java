c62a13e12b308f8b0e54c2de171f2bceedb3b36a
ianazimov <ianazimov@gmail.com>
2024-05-04 12:06:35 -0400

    Finished method for adding data, gonna work on updating now and reading through database to match with object

--- src/main/java/com/example/lostinthesauce/SignInController.java
+++ src/main/java/com/example/lostinthesauce/SignInController.java
@@ -1,5 +1,8 @@
 package com.example.lostinthesauce;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.WriteResult;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class SignInController {
     @FXML
@@ -19,55 +25,34 @@ public class SignInController {
 
     @FXML
     public TextField passwordTextField;
-    String dbFilePath = ".//AccountInfo.accdb";
-    String databaseURL = "jdbc:ucanaccess://" + dbFilePath;
-    Connection connect;
 
-    {
-        try {
-            connect = DriverManager.getConnection(databaseURL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public User currentUser;
 
-    public void printDatabase(){
-        dbClass.printAccountInfo(connect);
-    }
-
-
-    public void onLoginPress(ActionEvent event) throws IOException{
-        String usernameInput = usernameTextfield.getText();
-        String passwordInput = passwordTextField.getText();
-
-        try {
-            String tableName = "AccountInfo";
-            Statement stmt = connect.createStatement();
-            ResultSet result = stmt.executeQuery("select * from " + tableName);
-            while (result.next()) {
-                String username = result.getString("username");
-                String password = result.getString("password");
-                if(username.equals(usernameInput) && password.equals(passwordInput)){
-                    HelloApplication.setRoot("home-view");
-                }
-            }
-            System.out.println("Login called");
-        } catch (SQLException except) {
-            except.printStackTrace();
-        }
-
-    }
-    public void onCreateAccountPress(){
-        String username;
-        String password;
-        username = usernameTextfield.getText();
-        password = passwordTextField.getText();
-        dbClass.createAccount(connect, username, password);
-
-    }
     @FXML
-    private void switchToHome() throws IOException {
+    private void switchToHomeView() throws IOException {
         HelloApplication.setRoot("home-view");
+    }
+
+    public void addData() {
+
+        DocumentReference docRef = HelloApplication.fstore.collection("Users").document(UUID.randomUUID().toString());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("Username", usernameTextfield.getText());
+        data.put("Password", passwordTextField.getText());
+        data.put("Customization", 0);
+        data.put("Level 1 HiScore", 0);
+        data.put("Level 2 HiScore", 0);
+        data.put("Level 3 HiScore", 0);
+        data.put("Total HiScore", 0);
+
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+    }
+
+
+    public void deleteData(){
+
     }
 
 }
---
+++ src/main/java/com/example/lostinthesauce/User.java
@@ -0,0 +1,85 @@
+package com.example.lostinthesauce;
+
+public class User {
+        private String username;
+
+        private String password;
+
+        private int hiscore1;
+
+        private int hiscore2;
+
+        private int hiscore3;
+
+        private int hiscoreTotal;
+
+        private int charCustom;
+
+        public User(String username, String password, int hiscsore1, int hiscore2, int hiscore3, int hiscoreTotal, int charCustom){
+            this.charCustom = charCustom;
+            this.hiscore1 = hiscsore1;
+            this.hiscore2 = hiscore2;
+            this.hiscore3 = hiscore3;
+            this.hiscoreTotal = hiscoreTotal;
+            this.username=username;
+            this.password=password;
+        }
+
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public int getHiscore1() {
+            return hiscore1;
+        }
+
+        public void setHiscore1(int hiscore1) {
+            this.hiscore1 = hiscore1;
+        }
+
+        public int getHiscore2() {
+            return hiscore2;
+        }
+
+        public void setHiscore2(int hiscore2) {
+            this.hiscore2 = hiscore2;
+        }
+
+        public int getHiscore3() {
+            return hiscore3;
+        }
+
+        public void setHiscore3(int hiscore3) {
+            this.hiscore3 = hiscore3;
+        }
+
+        public int getHiscoreTotal() {
+            return hiscoreTotal;
+        }
+
+        public void setHiscoreTotal(int hiscoreTotal) {
+            this.hiscoreTotal = hiscoreTotal;
+        }
+
+        public int getCharCustom() {
+            return charCustom;
+        }
+
+        public void setCharCustom(int charCustom) {
+            this.charCustom = charCustom;
+        }
+}
